72eca7d3db839b81f73f4de4600c05bbecd524cf
Gurmali <Gurmali@hotmail.com>
2022-12-10 13:47:53 +0000

    updating the state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you" name="paul" age="old" />
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Paul" /> 
+      <StatefulGreetingWithCallback greeting="I'm a stateful class component!(with Call Back)" name="Paul" />
+      <StatefulGreetingWithPrevState greeting="I'm a stateful class component!(With Previous State)" name="Paul" />
     </div>
   );
 }
